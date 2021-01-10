9ea4c6c048f55f1f92c4dba4536ab16f2d7b208a
ch8n <chetan.garg36@gmail.com>
2021-01-10 15:52:37 +0530

    added/async

--- app/src/main/java/dev/ch8n/handlerexample/DownloadHandlerThread.kt
+++ app/src/main/java/dev/ch8n/handlerexample/DownloadHandlerThread.kt
@@ -1,100 +1,61 @@
 package dev.ch8n.handlerexample
 
-import android.content.Context
-import android.os.*
+import android.os.Handler
+import android.os.HandlerThread
+import android.os.Looper
 import android.util.Log
-import androidx.loader.content.AsyncTaskLoader
-import java.util.*
 
-class DownloadHandlerThread : Thread() {
 
-    lateinit var downloadHandler: DownloadHandler
-
-    init {
-        this.start()
-    }
-
-    override fun run() {
-        super.run()
-        Looper.prepare()
-        downloadHandler = DownloadHandler()
-        Looper.loop()
-    }
-
-    operator fun component1() = downloadHandler
-
+interface IAsync<Params, Progress, Result> {
+    fun doInBackground(vararg many: Params): Result
+    fun onPostExecute(data: Result)
+    fun onPreExecute()
+    fun onProgressUpdate(progress: Progress)
+    fun execute()
 }
 
-class DownloadHandler : Handler() {
 
-    fun execute(task: Runnable): DownloadHandler {
-        this.post(task)
-        return this
-    }
-
-    fun exit() {
-        this.looper.quit()
-    }
-
-}
-
-class DemoHandlerThread : HandlerThread("DemoThreadHandler") {
+class WorkerHandler : HandlerThread("worker thread!") {
 
     private val handler: Handler
 
     init {
         this.start()
-        // getLopper is blocking call
         handler = Handler(looper)
     }
 
-    fun execute(task: Runnable): DemoHandlerThread {
-        handler.post(task)
+
+    fun execute(task: () -> Unit): WorkerHandler {
+        handler.post {
+            task.invoke()
+        }
         return this
     }
-}
-
-class LongWork : Runnable {
-
-    var workId: String = UUID.randomUUID().toString()
-
-    override fun run() {
-        Log.d("Executor", "started workID:$workId on ${Thread.currentThread().name}")
-        Thread.sleep(4000)
-        Log.d("Executor", "completed workID:$workId on ${Thread.currentThread().name}")
-    }
 
 }
 
 
-//AsyncTaskLoader<[ReturnType]>
-class AsyncLoader(context: Context) : AsyncTaskLoader<String>(context) {
+abstract class AsyncResolver<Params, Progress, Result> : IAsync<Params, Progress, Result>, Handler(Looper.getMainLooper()) {
 
-    override fun loadInBackground(): String? {
+    private val workerHandler = WorkerHandler()
 
-        Log.d("AsyncLoader", "started on ${Thread.currentThread().name}")
-        Thread.sleep(4000)
-        Log.d("AsyncLoader", "completed on ${Thread.currentThread().name}")
-
-        return null
-    }
-
-}
-
-
-class AsyncWithDataLoader(context: Context, private val data: Bundle?) :
-    AsyncTaskLoader<String>(context) {
-
-    override fun loadInBackground(): String? {
-        val work = data?.getString("test")
-        Log.d("AsyncLoader", "started work:$work on ${Thread.currentThread().name}")
-        Thread.sleep(4000)
-        Log.d("AsyncLoader", "completed work:$work on ${Thread.currentThread().name}")
-        return work
-    }
-
-    override fun deliverResult(data: String?) {
-        val result = "$data also updated in delivery result";
-        super.deliverResult(result)
+    override fun execute() {
+        val parentHandler = this
+        println("execute on ${Thread.currentThread().name}")
+        parentHandler.post { onPreExecute() }
+        workerHandler.execute {
+            println("inside worker handler start on ${Thread.currentThread().name}")
+            val result: Result = doInBackground()
+            println("inside worker handler end on ${Thread.currentThread().name}")
+            parentHandler.post {
+                onPostExecute(result)
+            }
+        }
     }
 }
+
+
+
+
+
+
--- app/src/main/java/dev/ch8n/handlerexample/MainActivity.kt
+++ app/src/main/java/dev/ch8n/handlerexample/MainActivity.kt
@@ -1,144 +1,47 @@
 package dev.ch8n.handlerexample
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.os.Message
-import android.util.Log
-import android.widget.Toast
+import android.os.HandlerThread
 import androidx.appcompat.app.AppCompatActivity
-import androidx.loader.app.LoaderManager
-import androidx.loader.content.Loader
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.content_main.*
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var mainActHandler: Handler
-    private lateinit var loaderManager: LoaderManager
-
-    private lateinit var executor: ExecutorService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        mainActHandler = object : Handler(Looper.getMainLooper()) {
-            override fun handleMessage(msg: Message?) {
-                super.handleMessage(msg)
-                when {
-                    msg?.obj != null -> {
-                        result.text = msg.obj.toString()
-                    }
-                    msg?.data != null -> {
-                        result.text = msg.data.getString("test")
-                    }
-                }
+        @SuppressLint("HandlerLeak")
+        val task = object : AsyncResolver<String, Int, String>() {
+
+            override fun doInBackground(vararg many: String): String {
+
+                println("execute doInBackground on ${Thread.currentThread().name} == Worker")
+                Thread.sleep(1000)
+                println("execute on ${Thread.currentThread().name}")
+
+                return "done"
             }
-        }
 
-        val downloadHandlerThread = DownloadHandlerThread()
+            override fun onPostExecute(data: String) {
+                println("execute onPostExecute on ${Thread.currentThread().name}  == UI")
+            }
 
-        fab.setOnClickListener { view ->
-            val (downloadHandler) = downloadHandlerThread
+            override fun onPreExecute() {
+                println("execute onPreExecute on ${Thread.currentThread().name} == UI ")
+            }
 
-            downloadHandler.execute(Runnable {
-                Log.d("DownloadHandler", "${Thread.currentThread().name} executed")
-            }).execute(Runnable {
-                mainActHandler.sendMessage(Message().also {
-                    it.obj = "Pokemon downloaded"
-                })
-            })
-        }
-
-
-        val demoHandlerThread = DemoHandlerThread()
-
-        //overrding for demoHandlerThread
-        fab.setOnClickListener { view ->
-            demoHandlerThread.execute(Runnable {
-                Log.d("DownloadHandler", "${Thread.currentThread().name} executed")
-            }).execute(Runnable {
-                mainActHandler.sendMessage(Message.obtain().also {
-                    it.data = Bundle().also {
-                        it.putString("test", "pokemonResult")
-                    }
-                })
-            })
-
-        }
-
-
-        executor = Executors.newFixedThreadPool(5)
-
-        //overrding for executor example
-        fab.setOnClickListener { view ->
-
-            for (i in 0..10) {
-                executor.execute(LongWork())
+            override fun onProgressUpdate(progress: Int)  = runOnUiThread(){
+                println("execute onProgressUpdate on ${Thread.currentThread().name} == Worker")
             }
 
         }
 
-
-
-
-        loaderManager = LoaderManager.getInstance(this)
-        val loader = loaderManager.initLoader(
-            100,
-            Bundle.EMPTY,
-            object : LoaderManager.LoaderCallbacks<String> {
-                override fun onCreateLoader(id: Int, args: Bundle?): Loader<String> {
-                    return AsyncLoader(this@MainActivity)
-                }
-
-                override fun onLoadFinished(loader: Loader<String>, data: String?) {
-
-                }
-
-                override fun onLoaderReset(loader: Loader<String>) {
-
-                }
-
-            })
-
-        val loaderWithData = loaderManager.initLoader(
-            101,
-            Bundle().also {
-                it.putString("test", "pokemon")
-            },
-            object : LoaderManager.LoaderCallbacks<String> {
-                override fun onCreateLoader(id: Int, args: Bundle?): Loader<String> {
-                    return AsyncWithDataLoader(this@MainActivity, args)
-                }
-
-                override fun onLoadFinished(loader: Loader<String>, data: String?) {
-                    result.text = data
-                    Log.d("AsyncLoader", data)
-                }
-
-                override fun onLoaderReset(loader: Loader<String>) {
-
-                }
-
-            })
-
-        //overrding for asyncLoading example
-        fab.setOnClickListener { view ->
-            //loader.forceLoad()
-            loaderWithData.forceLoad()
+        fab.setOnClickListener {
+            task.execute()
         }
-
-
-    }
-
-
-    override fun onDestroy() {
-        super.onDestroy()
-        executor.shutdown()
     }
 
 
