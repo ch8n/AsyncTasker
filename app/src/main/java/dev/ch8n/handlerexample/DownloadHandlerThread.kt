d3a8443ac528a19dda2164e948f0d405a63144d1
ch8n <chetan.garg36@gmail.com>
2021-01-11 03:26:53 +0530

    customAsyncTask

--- app/src/main/java/dev/ch8n/handlerexample/DownloadHandlerThread.kt
+++ app/src/main/java/dev/ch8n/handlerexample/DownloadHandlerThread.kt
@@ -3,7 +3,6 @@ package dev.ch8n.handlerexample
 import android.os.Handler
 import android.os.HandlerThread
 import android.os.Looper
-import android.util.Log
 
 class WorkerHandler : HandlerThread("worker thread!") {
 
@@ -30,22 +29,33 @@ abstract class AsyncResolver<Params, Progress, Result> {
     abstract fun doInBackground(vararg many: Params): Result
     abstract fun onPostExecute(data: Result)
     abstract fun onPreExecute()
+    abstract fun onCompleted(isInterrupted: Boolean)
     abstract fun onProgressUpdate(progress: Progress)
 
-    private val workerHandler = WorkerHandler()
+
+    private val _workerHandler = WorkerHandler()
+    private var _isInterrupted: Boolean = false
 
     fun execute(vararg many: Params) {
         val parentHandler = Handler(Looper.getMainLooper())
         println("execute on ${Thread.currentThread().name}")
         parentHandler.post { onPreExecute() }
-        workerHandler.execute {
+        _workerHandler.execute {
             println("inside worker handler start on ${Thread.currentThread().name}")
-            val result: Result = doInBackground(*many)
-            println("inside worker handler end on ${Thread.currentThread().name}")
-            parentHandler.post {
-                onPostExecute(result)
+            val result = runCatching { doInBackground(*many) }.getOrNull()
+            _isInterrupted = result == null
+            if (result != null) {
+                parentHandler.post { onPostExecute(result) }
             }
+            parentHandler.post { onCompleted(_isInterrupted) }
+            println("end of worker on ${Thread.currentThread().name}")
         }
+    }
+
+
+    fun cancel() {
+        _workerHandler.interrupt()
+        _isInterrupted = true
     }
 }
 
--- app/src/main/java/dev/ch8n/handlerexample/MainActivity.kt
+++ app/src/main/java/dev/ch8n/handlerexample/MainActivity.kt
@@ -17,29 +17,46 @@ class MainActivity : AppCompatActivity() {
         val task = object : AsyncResolver<String, Int, String>() {
 
             override fun doInBackground(vararg many: String): String {
-                Thread.sleep(2000)
+                println("started doing work on ${Thread.currentThread().name}")
+                (0..100).forEach {
+                    onProgressUpdate(it)
+                    Thread.sleep(100)
+                }
                 println("completed on ${Thread.currentThread().name}")
 
                 return "done"
             }
 
             override fun onPostExecute(data: String) {
-                println("inside onPostExecute on ${Thread.currentThread().name}  == UI")
+
+            }
+
+            override fun onCompleted(isInterrupted: Boolean) {
+                println("asyncTask onCompleted on ${Thread.currentThread().name} $isInterrupted")
             }
 
             override fun onPreExecute() {
                 println("inside onPreExecute on ${Thread.currentThread().name} == UI ")
             }
 
-            override fun onProgressUpdate(progress: Int)  = runOnUiThread(){
-                println("inside onProgressUpdate on ${Thread.currentThread().name} == Worker")
+            override fun onProgressUpdate(progress: Int) = runOnUiThread() {
+                println("inside onProgressUpdate on ${Thread.currentThread().name} $progress")
             }
 
         }
 
+
         fab.setOnClickListener {
             task.execute()
         }
+
+        fab.setOnLongClickListener {
+            println("cancelled task.......")
+            task.cancel()
+            true
+        }
+
+
     }
 
 
